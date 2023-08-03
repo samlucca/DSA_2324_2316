7c7a568535add02020085fba0bbbd2acbdcf6ae6
yash2316 <141305637+yash2316@users.noreply.github.com>
2023-08-03 12:24:01 +0530

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,10 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define MAX_SIZE 100
+
+void is_empty(Stack* stack);
+int is_full(Stack* stack);
+int is_full(Stack* stack);
+
+#endif
---
+++ stack1.c
@@ -0,0 +1,16 @@
+#inlcude "Stack.h"
+
+//Initialise the stack
+void is_empty(Stack* stack){
+	stack->top=-1;
+}
+
+//check if the stack is empty
+int is_full(Stack* stack){
+	return stack->top == -1;
+}
+
+//check if the stack is full
+int is_full(Stack* stack){
+	return stack->top==MAX_SIZE-1;
+}
