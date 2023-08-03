211485f37802227932c8c54a2a8f747199a3a88a
yash2316 <141305637+yash2316@users.noreply.github.com>
2023-08-03 12:35:18 +0530

    Add files via upload

---
+++ Menu.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "utils.h"
+#include "stack.h"
+
+int main(){
+	int choice;
+int num = 0;
+
+while(num==0){
+	printf(" 1. Insertion Sort \n 2. Bubble sort \n 3. Selection sort \n4. Addition");
+	printf(" Enter your choice");
+	scanf("%d", &choice);
+	
+	switch (choice){
+		case 1:printf(" Insertion sort\n");
+				break;
+		case 2:printf(" Bubble sort\n");
+				break;
+		case 3:printf(" Selection sort\n");
+				break;
+		case 4:printf(" Addition of 5 and 9 = %d\n",add(5,9));
+				break;
+		case 0:num=1;
+				break;
+		default:printf(" Enter valid choice\n");
+		}
+	}
+	printf(" exited the menu");
+	return 0;
+	
+}
--- stack.h
+++ stack.h
@@ -2,9 +2,15 @@
 #define STACK_H
 
 #define MAX_SIZE 100
+typedef struct{
+	int data[MAX_SIZE];
+	int top;
+}Stack;
 
 void is_empty(Stack* stack);
 int is_full(Stack* stack);
 int is_full(Stack* stack);
+void push(Stack* stack,int item);
+int pop(Stack* stack);
 
 #endif
